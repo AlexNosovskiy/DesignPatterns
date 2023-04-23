bc429303dd7d4558b2e1efde9539251a0bdab141
AlexNosovskiy <stnihlatak@gmail.com>
2023-04-23 17:33:56 +0300

    Implement Builder pattern

--- src/main/java/creational/builder/Client.java
+++ src/main/java/creational/builder/Client.java
@@ -1,4 +1,13 @@
 package creational.builder;
 
 public class Client {
+    public static void main(String[] args) {
+
+        Student firstStudent = Student.builder()
+                .firstName("Mykola")
+                .lastName("Dnonnikov")
+                .build();
+
+        firstStudent.printStudent();
+    }
 }
